aac74f6d7ca28b9bca1050bdd14504da6802909f
Victor Forato <victorfdt@yahoo.com.br>
2019-03-15 16:11:07 +0100

    Setting another Bean id at @Qualifier

--- src/main/java/com/elros/spring/demo/annotations/TennisCoach.java
+++ src/main/java/com/elros/spring/demo/annotations/TennisCoach.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
 
 	@Autowired
-	@Qualifier("happyFortuneService")
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 
 	@Override
