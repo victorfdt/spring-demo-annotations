4d85b28035db5f30f7c33990601a32cb3f362bd6
Victor Di Trani <victorfdt@yahoo.com.br>
2019-03-18 11:59:02 +0000

    Lifecycle methods and annotations @PosConstruct and @PreDestroy

--- src/main/java/com/elros/spring/demo/annotations/TennisCoach.java
+++ src/main/java/com/elros/spring/demo/annotations/TennisCoach.java
@@ -5,9 +5,10 @@
  */
 package com.elros.spring.demo.annotations;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,28 +16,48 @@ import org.springframework.stereotype.Component;
  * @author victorfdt
  */
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
-	@Autowired
-	@Qualifier("randomFortuneService")
-	private FortuneService fortuneService;
+    @Autowired
+    @Qualifier("randomFortuneService")
+    private FortuneService fortuneService;
 
-	@Override
-	public String getDailyWorkout() {
-		return "Practice your backand volley";
-	}
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside constructor.");
+    }
 
-	/**
-	 * The return implementation will be HappyFortuneService, because this is the
-	 * only implementation of the interface FortuneService in the container/object
-	 * factory.
-	 * 
-	 * @return
-	 */
-	@Override
-	public String getDailyFortune() {
-		return fortuneService.getFortune();
-	}
+    /**
+     * It will execute AFTER the constructor and BEFORE the dependency
+     * injection.
+     */
+    @PostConstruct
+    public void doMySartupStuff() {
+        System.out.println(">> TennisCoach: inside doMyStartupStuff()");
+    }
+
+    /**
+     * It will be executed before the bean is destroyed
+     */
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: inside doMyCleanupStuff()");
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backand volley";
+    }
+
+    /**
+     * The return implementation will be HappyFortuneService, because this is
+     * the only implementation of the interface FortuneService in the
+     * container/object factory.
+     *
+     * @return
+     */
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
 
 }
